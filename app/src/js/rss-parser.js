7723ecc3879f9d1ccb2ccef3ccde1e6deb66a8cf
lapprand <lapprand@gmail.com>
2018-10-15 21:24:03 -0300

    Add CORS proxy to RSS parser

--- app/src/js/rss-parser.js
+++ app/src/js/rss-parser.js
@@ -1,13 +1,14 @@
 let Parser = require('rss-parser');
 let parser = new Parser();
- 
+const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
+
 (async () => {
- 
-  let feed = await parser.parseURL('https://nyaa.si/?page=rss&c=1_2&f=2');
+
+  let feed = await parser.parseURL(CORS_PROXY + 'https://nyaa.si/?page=rss&c=1_2&f=2');
   console.log(feed.title);
- 
+
   feed.items.forEach(item => {
     console.log(item.title + ':' + item.link)
   });
- 
+
 })();
