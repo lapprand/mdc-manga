d712a62be9635a9c9e7b2853e28404aa4b1c0a7f
lapprand <lapprand@gmail.com>
2018-10-16 22:15:20 -0300

    Changes to overall style and begin transition to other feed

---
+++ app/src/ts/new-card-cell-node.ts
@@ -0,0 +1,53 @@
+import { addHover } from "./mdc-hover";
+
+var newDiv = (...tokens: string[]) => {
+    let div = document.createElement("div") as HTMLDivElement;
+    tokens.forEach(token => { div.classList.add(token); });
+    return div;
+};
+
+export function newCardCellNode(item: any) {
+
+    // instantiate elements
+    let gridCell = newDiv("mdc-layout-grid__cell");
+
+    let card = newDiv("mdc-card");
+
+    let hoverWrapper = newDiv("hover", "mdc-elevation-transition");
+    addHover(hoverWrapper, "mdc-elevation--z1", "mdc-elevation--z24");
+
+    let cardPrimary = newDiv("mdc-card__primary");
+
+    let cardTitle = newDiv("card-title", "mdc-typography--headline6");
+
+    let title = document.createTextNode(item.title);
+
+    let cardActions = newDiv("mdc-card__actions");
+    let cardActionIcons = newDiv("mdc-card__action-icons");
+
+    let anchor = document.createElement("a") as HTMLAnchorElement;
+    // anchor.setAttribute("href", "magnet:?xt=urn:btih:" + item.hash);
+    anchor.setAttribute("href", item.link);
+
+    let iconButton = document.createElement("button") as HTMLButtonElement;
+    iconButton.classList.add("mdc-icon-button", "mdc-card__action", "mdc-card__action-icon");
+    iconButton.setAttribute("aria-pressed", "false");
+    iconButton.setAttribute("aria-label", "Magnet link");
+
+    let magnetIcon = document.createElement("i") as HTMLIFrameElement;
+    magnetIcon.classList.add("fas", "fa-magnet");
+
+    // append elements in order
+    iconButton.appendChild(magnetIcon);
+    anchor.appendChild(iconButton);
+    cardActionIcons.appendChild(anchor);
+    cardActions.appendChild(cardActionIcons);
+    cardTitle.appendChild(title);
+    cardPrimary.appendChild(cardTitle);
+    hoverWrapper.appendChild(cardPrimary);
+    hoverWrapper.appendChild(cardActions);
+    card.appendChild(hoverWrapper);
+    gridCell.appendChild(card);
+
+    return gridCell;
+};
--- app/src/ts/rss-parser.ts
+++ app/src/ts/rss-parser.ts
@@ -1,4 +1,14 @@
-import { addHover } from "./mdc-hover";
+import { newCardCellNode } from "./new-card-cell-node";
+
+const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
+let Parser = require("rss-parser");
+let parser = new Parser({
+    // customFields: {
+    //     item: [
+    //         ["nyaa:infoHash", "hash"]
+    //     ],
+    // }
+});
 
 // get node where items will be placed
 let grid = document.querySelector("#grid");
@@ -8,45 +18,13 @@ let innerDiv = document.createElement("div");
 innerDiv.classList.add("mdc-layout-grid__inner");
 grid.appendChild(innerDiv);
 
-var newDiv = (...tokens: string[]) => {
-    let div = document.createElement("div") as HTMLDivElement;
-    tokens.forEach(token => { div.classList.add(token); });
-    return div;
-};
-
-function newCardCellNode(item: any) {
-    let gridCell = newDiv("mdc-layout-grid__cell");
-
-    let card = newDiv("mdc-card", "mdc-card--outlined", "mdc-card__media--square");
-
-    let hoverWrapper = newDiv("hover", "mdc-elevation--z0", "mdc-elevation-transition");
-    addHover(hoverWrapper, "mdc-elevation--z0", "mdc-elevation--z8");
-
-    let cardPrimary = newDiv("mdc-card__primary");
-
-    let cardTitle = newDiv("card-title");
-
-    let title = document.createTextNode(item.title);
-
-    cardTitle.appendChild(title);
-    cardPrimary.appendChild(cardTitle);
-    hoverWrapper.appendChild(cardPrimary);
-    card.appendChild(hoverWrapper);
-    gridCell.appendChild(card);
-
-    return gridCell;
-};
-
-
-
-let Parser = require("rss-parser");
-let parser = new Parser();
-const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
 
 (async () => {
 
-    let feed = await parser.parseURL(CORS_PROXY + "https://nyaa.si/?page=rss&c=1_2&f=2");
+    // let feed = await parser.parseURL(CORS_PROXY + "https://nyaa.si/?page=rss&c=1_2&f=2");
+    let feed = await parser.parseURL(CORS_PROXY + "http://www.horriblesubs.info/rss.php?res=720");
 
+    (document.querySelector("#grid") as HTMLElement).style.display = "grid";
     feed.items.forEach((item: any) => {
         innerDiv.appendChild(newCardCellNode(item));
     });
