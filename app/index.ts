773b9172d0dc62e8548d20639d0f1da45097e4c2
lapprand <lapprand@gmail.com>
2018-10-29 16:17:20 -0300

    Add interpolation for determining font-size

--- app/index.ts
+++ app/index.ts
@@ -36,16 +36,13 @@ function fetchMoreItems() {
 // get node where items will be placed
 let grid = document.querySelector("#grid") as HTMLElement;
 
-// add layout-grid inner for cell wrapping
-// let innerDiv = document.createElement("div");
-// innerDiv.classList.add("mdc-layout-grid__inner");
-// grid.appendChild(innerDiv);
 
+let scrollHeight, scrollTop, clientHeight;
 function checkForNextPage() {
-    let scrollHeight = document.body.scrollHeight || document.documentElement.scrollHeight;
-    let scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
-    let clientHeight = window.innerHeight;
-    console.log(scrollTop + "+" + clientHeight + " = " + (scrollTop + clientHeight) + " = " + scrollHeight);
+    scrollHeight = document.body.scrollHeight || document.documentElement.scrollHeight;
+    scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+    clientHeight = window.innerHeight;
+    // console.log(scrollTop + "+" + clientHeight + " = " + (scrollTop + clientHeight) + " = " + scrollHeight);
     if (scrollTop + clientHeight >= scrollHeight - 400 && !fetching) {
         fetchMoreItems();
     }
